2e38808516cb63671b0add9181fb73d700f7d087
RedChiken <gombabo1217@gmail.com>
2017-02-17 01:16:09 +0900

    Add PawnArchetype

---
+++ Limbus/Source/Limbus/Private/PawnArchetype.cpp
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Limbus.h"
+#include "../Public/PawnArchetype.h"
+
+
+// Sets default values
+APawnArchetype::APawnArchetype()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APawnArchetype::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APawnArchetype::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APawnArchetype::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+
+// TODO : need to add map as a parameter
+// Called when player want to move
+TArray<FVector>* APawnArchetype::MovableArea(const FVector& target, const FVector& me) const {
+	TArray<FVector> *ret = nullptr;
+	for (auto i : moveRange) {
+		if (Movable(target, me)) {
+			(*ret).AddUnique(i);
+		}
+	}
+	return ret;
+}
+
+
+// TODO : need to add map as a parameter
+// Called when player want to attack
+TArray<FVector>* APawnArchetype::AttackableArea(const FVector& target, const FVector& me) const {
+	TArray<FVector> *ret = nullptr;
+	for (auto i : moveRange) {
+		if (Attackable(target, me)) {
+			(*ret).AddUnique(i);
+		}
+	}
+	return ret;
+}
+
+
+// Called in movableArea
+bool APawnArchetype::Movable(const FVector& target, const FVector& me) const {
+	bool ret = false;
+	for (auto i : moveRange) {
+		if (i == (target + me));
+		break;
+	}
+	return ret;
+}
+
+
+// Called in attackableArea
+bool APawnArchetype::Attackable(const FVector& target, const FVector& me) const {
+	bool ret = false;
+	for (auto i : attackRange) {
+		if (i == (target + me));
+		break;
+	}
+	return ret;
+}
---
+++ Limbus/Source/Limbus/Public/PawnArchetype.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "PawnArchetype.generated.h"
+
+UCLASS()
+class LIMBUS_API APawnArchetype : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APawnArchetype();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Called when player want to move
+	TArray<FVector>* MovableArea(const FVector& target, const FVector& me) const;
+
+	// Called when player want to attack
+	TArray<FVector>* AttackableArea(const FVector& target, const FVector& me) const;
+
+	// Called when pawn Intensified
+	virtual void Intensify();
+
+protected:
+	// Called in movableArea
+	bool Movable(const FVector& target, const FVector& me) const;
+	
+	// Called in attackableArea
+	bool Attackable(const FVector& target, const FVector& me) const;
+
+public:
+	TArray<UActorComponent *> skillset;
+	int HEALTH;
+	int ATK;
+	int SACRED;
+
+protected:
+	TArray<FVector> moveRange;
+	TArray<FVector> attackRange;
+	
+};
