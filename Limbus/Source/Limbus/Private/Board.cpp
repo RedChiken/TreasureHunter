acb645de765247295a17064ad7fad1a7432be9f3
RedChiken <gombabo1217@gmail.com>
2017-02-21 20:57:13 +0900

    add SpawnPawn function

--- Limbus/Source/Limbus/Private/Board.cpp
+++ Limbus/Source/Limbus/Private/Board.cpp
@@ -5,11 +5,27 @@
 #include "../Public/PawnArchetype.h"
 
 // Sets default values
-ABoard::ABoard()
+ABoard::ABoard(int mNum) : MAPNUMBER(mNum)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	for (int i = 0; i < MAPSIZE[MAPNUMBER]; i++) {
+		for (int j = 0; j < MAPSIZE[MAPNUMBER]; j++) {
+			SpawnMap(i, j);
+		}
+	}
+	/*test codes
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> PlaneMeshAsset
+	(TEXT("staticMesh'/Content/StarterContent/Shapes/Shape_Plane.uasset"));		//static mesh 경로를 받아 UStaticMesh 객체 생성
+	if (PlaneMeshAsset.Succeeded()) {
+		//this code should be remain
+		PrimaryActorTick.bCanEverTick = true;
+		PlaneVisualComponent = CreateDefaultSubobject<UStaticMeshComponent>
+			(TEXT("PlaneVisualComponent"));										//UStaticMeshComponent의 객체 생성?
+		PlaneVisualComponent->SetStaticMesh(PlaneMeshAsset.Object);				//static mesh 객체를 UStaticMeshComponet에 넣는다.
+		RootComponent = PlaneVisualComponent;									//맵에서 중심이 될 객체 설정
+		SetActorEnableCollision(true);											//이 객체가 collision이 되도록 설정
+	}
+	testcodes*/
 }
 
 // Called when the game starts or when spawned
@@ -28,8 +44,20 @@ void ABoard::Tick(float DeltaTime)
 
 
 // Set static mesh, shader.
-void ABoard::SpawnMap(int mapSize) {
-
+void ABoard::SpawnMap(int x, int y) {
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> planeMeshAsset
+	(TEXT("staticMesh'/Content/StarterContent/Shapes/Shape_Plane.uasset"));
+	if (planeMeshAsset.Succeeded()) {
+		PrimaryActorTick.bCanEverTick = true;
+		PlaneVisualComponentMap[x][y] = CreateDefaultSubobject<UStaticMeshComponent>
+			(TEXT("PlaneVisualComponent"));
+		PlaneVisualComponentMap[x][y]->SetRelativeLocation
+		(this->MAPPOSITION[MAPNUMBER] + FVector(x * 100.0f, y * 100.0f, 0.0f));
+		if ((x == (MAPSIZE[MAPNUMBER] + 1) / 2) && (y == (MAPSIZE[MAPNUMBER] / 2))) {
+			RootComponent = PlaneVisualComponentMap[x][y];
+		}
+		SetActorEnableCollision(true);
+	}
 }
 
 // Spawn Pawn
--- Limbus/Source/Limbus/Public/Board.h
+++ Limbus/Source/Limbus/Public/Board.h
@@ -12,7 +12,7 @@ class LIMBUS_API ABoard : public AActor
 		class APawnArchetype;
 public:	
 	// Sets default values for this actor's properties
-	ABoard();
+	ABoard(int mapSize);
 
 
 protected:
@@ -24,7 +24,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	// Called when map Spawned
-	void SpawnMap(int mapSize);
+	void SpawnMap(int x, int y);
 
 	// Spawn Pawn. Called when pawn spawned.
 	void SpawnPawn(FVector position, APawnArchetype* pawn);
@@ -33,10 +33,26 @@ public:
 	void MovePawn(FVector now, FVector target);
 
 	//TODO : attack pawn, skill pawn
+
+protected:
+	//testcodes
+	TArray< TArray<UStaticMeshComponent*> > PlaneVisualComponentMap;
+	UStaticMeshComponent* PlaneVisualComponent;
+
 private:
 	TMap<FVector, APawnArchetype*> pawnMap;
 	TSet<APawnArchetype*> deadPawn;
 	//TODO : need to make trap(int yet) clas
 	TMap<FVector, int*> trapMap;
-	
+	const int MAPNUMBER;
+	const TArray<FVector> MAPPOSITION = {
+		FVector(46870.37037f, 58651.63545f, 0.0f),
+		FVector(11423.07692f, 52063.9598f, 100.0f), 
+		FVector(-43961.53846f, -51551.16784f, 100.0f), 
+		FVector(9125.0f, -53995.88798f, 200.0f), 
+		FVector(-33062.5f, 35860.75535f, 200.0f), 
+		FVector(-47125.0f, 22648.05954f, 200.0f), 
+		FVector(-19750.0f, -52071.06264f, 300.0f)
+	};
+	const TArray<int> MAPSIZE = { 11, 13, 13, 17, 17, 17, 19 };
 };
