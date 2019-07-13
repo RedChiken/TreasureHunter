8c6a18ec3368c5e1e5059897a92018361f688657
RedChiken <computer6561@gmail.com>
2019-07-14 01:01:13 +0900

    Create Project

---
+++ Source/TreasureHunter/Private/TreasureHunter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TreasureHunter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TreasureHunter, "TreasureHunter" );
