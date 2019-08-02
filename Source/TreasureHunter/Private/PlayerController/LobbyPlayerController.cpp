6e394c130a8789a30e8783f38960a7ad6116f179
RedChiken <computer6561@gmail.com>
2019-08-03 07:14:46 +0900

    Implement LobbyPlayerController

--- Source/TreasureHunter/Private/PlayerController/LobbyPlayerController.cpp
+++ Source/TreasureHunter/Private/PlayerController/LobbyPlayerController.cpp
@@ -1,22 +1,30 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "LobbyPlayerController.h"
-#include "LobbyPlayerController.h"
+#include "WidgetControllerComponent.h"
 #include "TreasureHunter.h"
 #include "UnrealNetwork.h"
 
 ALobbyPlayerController::ALobbyPlayerController() {
-
+	WidgetController = CreateDefaultSubobject<UWidgetControllerComponent>(TEXT("WidgetController"));
 }
 
 void ALobbyPlayerController::Client_InitializeLobbyUI_Implementation()
 {
+	InitializeLobbyUI();
 }
 
-void ALobbyPlayerController::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutlifetimeProps) const
+void ALobbyPlayerController::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ALobbyPlayerController, WidgetController);
 }
 
 void ALobbyPlayerController::InitializeLobbyUI()
 {
+	if (bLobbyUIInitialized) {
+		UE_LOG(LogTemp, Log, TEXT("%hs: Already initialized"), __FUNCTION__);
+		return;
+	}
+	ReceiveInitializeLobbyUI();
 }
