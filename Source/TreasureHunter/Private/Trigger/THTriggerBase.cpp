e0256e6e79a71cba1c00a01dc4e672a051f09c18
RedChiken <computer6561@gmail.com>
2020-01-30 21:20:54 +0900

    Add Collision Trigger  to Test

---
+++ Source/TreasureHunter/Private/Trigger/THTriggerBase.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "THTriggerBase.h"
+#include "Components/BoxComponent.h"
+#include "Engine.h"
+#include "net/UnrealNetwork.h"
+#include "THCharacterBase.h"
+
+// Sets default values
+ATHTriggerBase::ATHTriggerBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Area = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractionArea"));
+	Area->BodyInstance.SetCollisionProfileName("InteractionArea");
+	Area->SetBoxExtent(Extent);
+	Area->AddRelativeLocation(Location);
+	Area->SetBoundsScale(5.0f);
+	Area->OnComponentBeginOverlap.AddDynamic(this, &ATHTriggerBase::OnOverlap);
+	//Area->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ATHTriggerBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATHTriggerBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ATHTriggerBase, Extent);
+	DOREPLIFETIME(ATHTriggerBase, Location);
+}
+
+// Called every frame
+void ATHTriggerBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATHTriggerBase::OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		auto Character = Cast<ATHCharacterBase>(OtherActor);
+		if (Character)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("Character gets in to the Interaction Area"));
+		}
+	}
+}
+
---
+++ Source/TreasureHunter/Public/Trigger/THTriggerBase.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "THTriggerBase.generated.h"
+
+UCLASS()
+class TREASUREHUNTER_API ATHTriggerBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATHTriggerBase();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
+
+private:
+	UFUNCTION()
+		void OnOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = InteractionArea)
+		class UBoxComponent* Area;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category = AreaSize, meta = (AllowPrivateAccess = "true"))
+		FVector Extent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category = AreaSize, meta = (AllowPrivateAccess = "true"))
+		FVector Location;
+};
