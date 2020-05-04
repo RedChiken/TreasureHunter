905e94bbc11a96df7d5f62efa0cbda88a866d1f5
RedChiken <computer6561@gmail.com>
2020-05-04 16:47:10 +0900

    Add new log category

--- Source/TreasureHunter/Public/TreasureHunter.h
+++ Source/TreasureHunter/Public/TreasureHunter.h
@@ -18,6 +18,13 @@ DECLARE_LOG_CATEGORY_EXTERN(LogTH_PlayerBase_IdleType, Verbose, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogTH_PlayerBase_CheckValue, Verbose, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogTH_PlayerBase_CheckOverlap, Verbose, All);
 
+DECLARE_LOG_CATEGORY_EXTERN(THError, Error, All);
+DECLARE_LOG_CATEGORY_EXTERN(THWarning, Warning, All);
+DECLARE_LOG_CATEGORY_EXTERN(THDisplay, Display, All);
+DECLARE_LOG_CATEGORY_EXTERN(THLog, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(THVerbose, Verbose, All);
+DECLARE_LOG_CATEGORY_EXTERN(THVeryVerbose, VeryVerbose, All);
+
 #define GETENUMSTRING(etype, evalue) ( (FindObject<UEnum>(ANY_PACKAGE, TEXT(etype), true) != nullptr) ? FindObject<UEnum>(ANY_PACKAGE, TEXT(etype), true)->GetNameStringByIndex((int32)evalue) : FString("Invalid - are you sure enum uses UENUM() macro?") )
 
 #define GETBOOLSTRING(bvalue) (bvalue ? TEXT("true") : TEXT("false"))
